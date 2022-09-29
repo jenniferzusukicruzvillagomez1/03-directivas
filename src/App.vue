b0a25fa5a9314305608f6dd53fe87071fbba8e3c
jenniferzusukicruzvillagomez1 <a3521110355@alumno.uttehuacan.edu.mx>
2022-09-29 01:01:07 -0500

    Segunda Version 03-directivas 12:49

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
 <div>
+  <h1>Universidad Tecnologica de Tehuacan</h1>
+  <h2>Aplicaciones Orientadas a la Web</h2>
+  <h3>Grupo 4B</h3>
   <h1>Directivas en VUE.js</h1>
   <hr />
 <h2>v-text</h2>
@@ -9,7 +12,8 @@
 <br>
 <span v-text= "titulo"></span>
 <br>Esta es una trasmutacion de texto {{titulo}}, con una variable prop <br>
-
+<span v-text= "titulo"></span>
+<br><br>
 
 </div>
 </template>
